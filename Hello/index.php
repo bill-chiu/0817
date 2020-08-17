fc1d136a19955d57ad761ee94b68ee9c55e1c9c3
CS01183 <68837493+bill-chiu@users.noreply.github.com>
2020-08-17 14:13:16 +0800

    可以從跳轉到hello

---
+++ Hello/index.php
@@ -0,0 +1,22 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    
+    
+    , initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="POST" action="hello.php">
+    your name:
+    <input type="text" name="txtUserName">   
+    <!-- <input type="text" name="txtUserNick">    -->
+    <input type="submit" name="btnok" value="OK">   
+    </form>
+</body>
+</html>
